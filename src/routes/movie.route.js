4c5c73fc743aa1e65efd33b8f5a494acd9e35534
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2024-11-03 10:40:22 +0700

    middleware auth

--- src/routes/movie.route.js
+++ src/routes/movie.route.js
@@ -1,6 +1,9 @@
 import express from "express";
 import * as movieController from "../controllers/movie.controller.js";
+import { protectRoute } from "../middlewares/protectRoute.middleware.js";
 const router = express.Router();
+
+router.use(protectRoute);
 
 router.get("/trending", movieController.getTrending);
 router.get("/:id/trailers", movieController.getMovieTrailer);
--- src/routes/tvshow.route.js
+++ src/routes/tvshow.route.js
@@ -1,6 +1,9 @@
 import express from "express";
 import * as tvshowController from "../controllers/tvshow.controller.js";
+import { protectRoute } from "../middlewares/protectRoute.middleware.js";
 const router = express.Router();
+
+router.use(protectRoute);
 
 router.get("/trending", tvshowController.getTrending);
 router.get("/:id/trailers", tvshowController.getTvTrailer);
